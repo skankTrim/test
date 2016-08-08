81fbba35cade240639059b23c4cdbc8bedd6bc62
skank <skank22@yandex.ru>
2016-08-08 15:27:05 +0400

    test

---
+++ company.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ru" xml:lang="ru" prefix="umi: http://www.umi-cms.ru/TR/umi">
+<head>
+	<title>О компании</title>
+	<? include('inc/head.php'); ?>
+	<? include('inc/scripts.php'); ?>
+</head>
+<body id="company">
+	<!-- <img src="uploads/header.jpg" alt="" class="screen"> -->
+<div id="root">
+	<div id="wrapper">
+		<? include('inc/header.php'); ?>
+		<div id="content">
+			<div class="wrapper">
+				<div class="bread_crumbs">
+					<ul>
+						<li><a href="#">Главная</a></li>
+						<li class="current"> / <a href="#">О компании</a></li>
+					</ul>
+				</div>
+				<div class="title">
+					<h2>О компании</h2>
+				</div>
+				<div class="content-menu">
+					<ul>
+						<li><a href="#" class="">Стратегия развития</a></li>
+						<li><a href="#" class="">Руководство</a></li>
+						<li><a href="#" class="">Клиенты</a></li>
+						<li><a href="#" class="">Партнёры</a></li>
+						<li><a href="#" class="">Отзывы и рекомендации</a></li>
+						<li><a href="#" class="">Вакансии</a></li>
+						<li><a href="#" class="">Вопросы и ответы</a></li>
+					</ul>
+				</div>
+				<div class="about">
+					<div class="_left">
+						<p>Евроконсалт — это международная компания, предоставляющая услуги в сфере налогообложения, права, аудита и консалтинга 
+							и ведущая свою деятельность в Европе, Америке, Азии и России.</p>
+					</div>
+
+					<div class="_video"></div>
+					<div class="txt">
+						<p class="_txt_short">Группа компаний, в которую входит наша юридическая фирма основана в 1994 году.</p><br />
+						<p>Штат сотрудников сформирован из высококвалифицированных юристов, адвокатов с большим практическим опытом работы в федеральных органах законодательной 
+							и исполнительной власти Российской федерации – они составляют основу кадрового состава. Молодые юристы обладают глубокими академическими знаниями в
+							области гражданского, корпоративного, административного, уголовного и налогового права, они всегда в курсе любых даже самых незначительных изменений 
+							права и имеют уникальную возможность своевременно реагировать на любые неожиданные изменения законодательства в нашей стране.</p><br />
+						<p>Фирменным отличием нашей компании от других агентств, предоставляющих аналогичные услуги, является прежде всего комплексный, системный подход 
+							к решению проблем в частной жизни и бизнес – сфере. Создавая нашим клиентам бизнес с “нуля“ мы продолжаем оказывать юридические услуги созданным компаниям,
+							не оставляем наших предпринимателей один на один с проблемами в сложном и порой враждебном океане бизнеса, успешно решаем текущие правовые проблемы
+							в рамках &#171;комплексного правового обслуживания&#187;.</p>	
+					</div>
+					<div class="advantages">
+						<h4>Наши преимущества</h4>
+						<ul>
+							<li>
+								<img src="templates/ecg/img/ico/about-1.svg" alt="" />
+								<p><span>Решение всех вопросов в одном месте</span></p>
+							</li>
+							<li>
+								<img src="templates/ecg/img/ico/about-2.svg" alt="" />
+								<p><span>Поддержка по всему миру</span></p>
+							</li>
+							<li>
+								<img src="templates/ecg/img/ico/about-3.svg" alt="" />
+								<p><span>Личный менеджер</span></p>
+							</li>
+							<li>
+								<img src="templates/ecg/img/ico/about-4.svg" alt="" />
+								<p><span>Современные инструменты коммуникации</span></p>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="activities">
+					<h2>Наша специализация</h2>
+					<div class="services_block">
+						<ul class="justi">
+							<li>
+								<a href="#" class="link"></a>
+								<img src="templates/ecg/img/services_pic1.jpg" alt="" />
+								<img class="hover" src="templates/ecg/img/services_pic1-hover.jpg" alt="" />
+								<div class="txt"><span>Бухгалтерские услуги</span></div>
+							</li>
+							<li>
+								<a href="#" class="link"></a>
+								<img src="templates/ecg/img/services_pic2.jpg" alt="" />
+								<img class="hover" src="templates/ecg/img/services_pic2-hover.jpg" alt="" />
+								<div class="txt"><span>Юридические услуги</span></div>
+							</li>
+							<li>
+								<a href="#" class="link"></a>
+								<img src="templates/ecg/img/services_pic3.jpg" alt="" />
+								<img class="hover" src="templates/ecg/img/services_pic3-hover.jpg" alt="" />
+								<div class="txt"><span>Готовый бизнес</span></div>
+							</li>
+							<li>
+								<a href="#" class="link"></a>
+								<img src="templates/ecg/img/services_pic4.jpg" alt="" />
+								<img class="hover" src="templates/ecg/img/services_pic4-hover.jpg" alt="" />
+								<div class="txt"><span>Бизнес-справки</span></div>
+							</li>
+							<li>
+								<a href="#" class="link"></a>
+								<img src="templates/ecg/img/services_pic5.jpg" alt="" />
+								<img class="hover" src="templates/ecg/img/services_pic5-hover.jpg" alt="" />
+								<div class="txt"><span>Риэлторские услуги</span></div>
+							</li>
+							<li>
+								<a href="#" class="link"></a>
+								<img src="templates/ecg/img/services_pic6.jpg" alt="" />
+								<img class="hover" src="templates/ecg/img/services_pic6-hover.jpg" alt="" />
+								<div class="txt"><span>Услуги по переводу</span></div>
+							</li>
+							<li>
+								<a href="#" class="link"></a>
+								<img src="templates/ecg/img/services_pic7.jpg" alt="" />
+								<img class="hover" src="templates/ecg/img/services_pic7-hover.jpg" alt="" />
+								<div class="txt"><span>Консультации</span></div>
+							</li>
+							<li>
+								<a href="#" class="link"></a>
+								<img src="templates/ecg/img/services_pic8.jpg" alt="" />
+								<img class="hover" src="templates/ecg/img/services_pic8-hover.jpg" alt="" />
+								<div class="txt"><span>Оценочные услуги</span></div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- <iframe frameborder="0" id="post_form" name="post_form"></iframe> -->
+</div>
+<? include('inc/footer.php'); ?>
+</body>
+</html>
---
+++ vacancy.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ru" xml:lang="ru" prefix="umi: http://www.umi-cms.ru/TR/umi">
+<head>
+	<title>Вакансии</title>
+	<? include('inc/head.php'); ?>
+	<? include('inc/scripts.php'); ?>
+</head>
+<body id="vacancy">
+	<!-- <img src="uploads/header.jpg" alt="" class="screen"> -->
+<div id="root">
+	<div id="wrapper">
+		<? include('inc/header.php'); ?>
+		<div id="content">
+			<div class="wrapper">
+				<div class="bread_crumbs">
+					<ul>
+						<li><a href="#">Главная</a></li>
+						<li> / <a href="#">О компании</a></li>
+						<li class="current"> / <a href="#">Вакансии</a></li>
+					</ul>
+				</div>
+				<div class="title">
+					<h2>Карьера в компании &#171;Евроконсалт&#187;</h2>
+				</div>
+				<div class="career_block">
+					<div class="txt">
+						<p class="_top">Мы верим, что в мире есть масса возможностей заниматься тем, что по-настоящему нравится. И точно знаем, 
+							что это можно делать у нас, в хорошей компании.</p>
+						<p>Работа в Евроконсалт — это возможность реализовать себя в большой группе компаний и быть напрямую причастным
+							к созданию уникальных продуктов на российском медиарынке. Мы нанимаем лучших, чтобы учиться у них, 
+							обмениваться опытом и повышать экспертизу. В то же время нам интересно и выгодно вкладываться в развитие людей,
+							а за увлекательным процессом получать результат.</p>
+					</div>
+					<div class="slide-user">
+						<ul class="slides">
+							<li><img src="uploads/slide-user.jpg" alt="" /></li>
+							<li><img src="uploads/slide-user.jpg" alt="" /></li>
+							<li><img src="uploads/slide-user.jpg" alt="" /></li>
+							<li><img src="uploads/slide-user.jpg" alt="" /></li>
+							<li><img src="uploads/slide-user.jpg" alt="" /></li>
+						</ul>
+						<!-- <div><img src="uploads/slide-user.jpg" alt="" /></div>
+					 	<div><img src="uploads/slide-user.jpg" alt="" /></div>
+					 	<div><img src="uploads/slide-user.jpg" alt="" /></div>
+					 	<div><img src="uploads/slide-user.jpg" alt="" /></div>
+					 	<div><img src="uploads/slide-user.jpg" alt="" /></div> -->
+					</div>
+				</div>
+				<div class="our_vacancies">
+					<div class="title">
+						<h2>Наши вакансии</h2>
+					</div>
+					<div class="_list">
+						<div class="line_list">
+							<div class="wrap">
+								<span>Помощник юриста</span><ins class="down"></ins>
+							</div>
+							<div class="open_sublist">
+								<div class="item">
+									<h4>Обязанности</h4>
+									<ul>
+										<li>Помощь юристам в осуществлении проектов компании;</li>
+										<li>Составление документов по готовым шаблонам, а также разработка документов небольшой сложности под руководством юристов;</li>
+										<li>Подготовка (ксерокопирование, формирование пакетов) документов для подачи в суды общей юрисдикции, арбитражные суды,
+											отслеживание движения дел и иная помощь.</li>
+									</ul>
+								</div>
+								<div class="item">
+									<h4>Требования</h4>
+									<ul>
+										<li>Наличие отличных показателей и результатов в учебе;</li>
+										<li>Наличие теоретических знаний;</li>
+										<li>Ответственность и исполнительность;</li>
+										<li>Аналитический склад ума;</li>
+										<li>Грамотная русская письменная и устная речь;</li>
+										<li>Готовность к интенсивной работе.</li>
+									</ul>
+								</div>
+								<div class="item">
+									<h4>Условия</h4>
+									<ul>
+										<li>Офис находится в 50 метрах от станции метро;</li>
+										<li>График работы 5/2, 10.00-19.00;</li>
+										<li>Оформление согласно ТК РФ;</li>
+										<li>Возможности для карьерного и профессионального роста!</li>
+									</ul>
+								</div>
+								<div class="btn_resume">
+									<a href="#" class="trigger" rel="trigger">Отправить резюме</a>
+								</div>
+							</div>
+						</div>
+						<div class="line_list">
+							<div class="wrap">
+								<span>Помощник главного бухгалтера</span><ins class="down"></ins>
+							</div>	
+							<div class="open_sublist">
+								<div class="item">
+									<h4>Обязанности</h4>
+									<ul>
+										<li>Помощь юристам в осуществлении проектов компании;</li>
+										<li>Составление документов по готовым шаблонам, а также разработка документов небольшой сложности под руководством юристов;</li>
+										<li>Подготовка (ксерокопирование, формирование пакетов) документов для подачи в суды общей юрисдикции, арбитражные суды,
+											отслеживание движения дел и иная помощь.</li>
+									</ul>
+								</div>
+								<div class="btn_resume">
+									<a href="#" class="trigger" rel="trigger">Отправить резюме</a>
+								</div>
+							</div>
+						</div>
+						<div class="line_list">
+							<div class="wrap">
+								<span>Ведущий юрист</span><ins class="down"></ins>
+							</div>	
+							<div class="open_sublist">
+								<div class="item">
+									<h4>Обязанности</h4>
+									<ul>
+										<li>Помощь юристам в осуществлении проектов компании;</li>
+										<li>Составление документов по готовым шаблонам, а также разработка документов небольшой сложности под руководством юристов;</li>
+										<li>Подготовка (ксерокопирование, формирование пакетов) документов для подачи в суды общей юрисдикции, арбитражные суды,
+											отслеживание движения дел и иная помощь.</li>
+									</ul>
+								</div>
+								<div class="item">
+									<h4>Требования</h4>
+									<ul>
+										<li>Наличие отличных показателей и результатов в учебе;</li>
+										<li>Наличие теоретических знаний;</li>
+										<li>Ответственность и исполнительность;</li>
+										<li>Аналитический склад ума;</li>
+										<li>Грамотная русская письменная и устная речь;</li>
+										<li>Готовность к интенсивной работе.</li>
+									</ul>
+								</div>
+								<div class="btn_resume">
+									<a href="#" class="trigger" rel="trigger">Отправить резюме</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="themodal-overlay">
+				<div class="modal trigger modal_jobs">
+				    <a href="#" class="close"></a>
+				    <div class="form">
+				    	<form action="#">
+				    		<h4>Отправить резюме</h4>
+				    		<div class="_left">
+				    			<div class="select">
+				    				<p>Вакансия</p>
+				    				<select>
+				    					<option>Помощник юриста</option>
+				    					<option>Помощник главного бухгалтера</option>
+				    					<option>Ведущий юрист</option>
+				    				</select>
+				    			</div>
+				    			<div>
+				    				<label>
+					    				<div class="element">
+					    					<p>Контактный телефон</p>
+											<input type="text" class="phone" name="phone" />
+										</div>
+									</label>
+				    			</div>
+				    			<div>
+				    				<label>
+					    				<div class="element">
+					    					<p>Электронная почта</p>
+											<input type="text" class="email" name="email" />
+										</div>
+									</label>
+				    			</div>
+								<div>
+									<p>Ссылка на резюме</p>
+									<label class="file-upload">
+								        <span class="button">Обзор...</span>
+								        <mark></mark>
+								        <input type="file" class="filename" name="filename" />
+							   		</label>
+								</div>
+			    			</div>
+				    		<div class="_right">
+				    			<div>
+									<label>
+					    				<div class="element">
+					    					<p>ФИО</p>
+											<input type="text" class="fio" name="fio" />
+										</div>
+									</label>
+								</div>
+								<div class="textarea">
+									<label>
+									<p>Расскажите о себе</p>
+										<textarea></textarea>
+									</label>
+								</div>
+				    		</div>
+				    		<div class="btn_form">
+								<input type="submit" class="btn" value="Отправить" />
+							</div>
+				    	</form>	
+				    </div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	<!-- <iframe frameborder="0" id="post_form" name="post_form"></iframe> -->
+</div>
+<? include('inc/footer.php'); ?>
+</body>
+</html>
